b898cd1bdaca21c6eaedf3f365151c8d0f8c34e0
755719102 <755719102@qq.com>
2018-07-08 16:27:30 +0800

    second: 读取模块实现

--- log_process.go
+++ log_process.go
@@ -4,6 +4,10 @@ import (
 	"time"
 	"strings"
 	"fmt"
+	"os"
+	"bufio"
+	"io"
+	"path/filepath"
 )
 
 type Write interface {
@@ -11,7 +15,7 @@ type Write interface {
 }
 
 type Read interface {
-	read(rChan chan string)
+	read(rChan chan []byte)
 }
 
 //结构体作为对象的封装
@@ -19,7 +23,7 @@ type LogProcess struct {
 	r Read
 	w Write
 
-	rCh chan string // goroutine之间通信的通道，同步数据
+	rCh chan []byte // goroutine之间通信的通道，同步数据
 	wCh chan string
 	//path         string // 读取文件的路径
 	//influxdbinfo string // influx data source //类似于用户名密码ip等的信息
@@ -36,33 +40,59 @@ type WriteToInfluxdb struct {
 
 // 优化：只有这三个函数扩展性很差，只能从文件读写，可能还需要从标准输入输出里面读写，再定义模块太麻烦了，所以需要实现读写的接口，
 // 读取模块
-func (rf *ReadFromFile) read(rCh chan string) {
-	str := "message"
-	rCh <- str
+func (rf *ReadFromFile) read(rCh chan []byte) {
+	// 打开文件
+	f, err := os.Open(rf.path)
+	if err != nil {
+		dir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+		fmt.Printf("ERROR: 在当前目录：%s，打开文件出错%s", dir, err.Error())
+	}
+	// 从文件末尾开始逐行读取数据
+	f.Seek(0, 2)
+	// 这样可以逐行读取，并且效率比较高，因为会创建更大的buffer
+	rd := bufio.NewReader(f)
+	// 循环读取
+	for {
+		line, err := rd.ReadBytes('\n')
+		if err == io.EOF {
+			time.Sleep(500 * time.Millisecond)
+			continue
+		} else if err != nil {
+			panic(fmt.Sprintf("PANIC: 文件读取失败，%s", err.Error()))
+		}
+		rCh <- line[:len(line)-1]
+	}
+	//str := "message"
+	//rCh <- str
 }
 
 // 解析模块
 func (l *LogProcess) ParseFromRead() {
-	str := <-l.rCh
-	str = strings.ToUpper(str)
-	l.wCh <- str
+	// 循环解析
+	for str := range l.rCh {
+		l.wCh <- strings.ToUpper(string(str))
+	}
 }
 
 // 写入模块
 func (l *WriteToInfluxdb) write(wCh chan string) {
-	fmt.Println(<-wCh)
+	// 循环写入
+	for value := range wCh {
+		fmt.Println(value)
+	}
 }
 
 func main() {
 	loger := &LogProcess{
 		// 这里发现一定要使用&struct，不知道为什么。难道是直接实例化不会当做接口实现，一定要传引用？
 		r: &ReadFromFile{
-			path: "/var/log/messages/",
+			//path: "D:/mygo/src/Log-Analysis/log.txt",
+			path: "./log.txt",
 		},
 		w: &WriteToInfluxdb{
 			influxdbinfo: "root&passworld&ip",
 		},
-		rCh:  make(chan string),
+		rCh: make(chan []byte),
 		wCh: make(chan string),
 		//path:         "/var/log/messages/",
 		//influxdbinfo: "passworld&root",
@@ -72,5 +102,5 @@ func main() {
 	go loger.ParseFromRead()
 	go loger.w.write(loger.wCh)
 
-	time.Sleep(1 * time.Second)
+	time.Sleep(60 * time.Second)
 }
